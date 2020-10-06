2b6d3e03571a48a63fcfa56d932f99a45d751e52
frankfurt school of life <bla>
2020-10-06 15:35:39 +0200

    simplified timers and added pid

--- foil-actuator-simple.ino
+++ foil-actuator-simple.ino
@@ -14,10 +14,27 @@
 DualVNH5019MotorShield motor_shield;
 
 volatile int encoder_pulses = 0;
+int last_encoder_pulsed = encoder_pulses;
 
 static volatile int encoder_pulses_prev = encoder_pulses;
 static volatile u32 systick_count = 0;
 static volatile float speed_pulses_per_sec = 0.0;
+
+int pwm_min
+
+int delay_time = 100;
+
+
+
+float p_term = 2;
+float i_term = 0.0;
+float d_term = 0.02;
+
+float i_term_result = 0;
+
+float out_min = -300, out_max = 300;
+
+int setpoint = 2000;
 
 //TIM_TypeDef *tim_enc = TIM2;
 //TIM_TypeDef *tim_speed = TIM2;
@@ -40,7 +57,6 @@ void encoder_ISR(){
 
 
 void calc_speed() {
-
         // perform speed calculation
         if (speed_calc_data.idx++ == speed_calc_data.len){
             speed_calc_data.idx = 0;
@@ -55,7 +71,32 @@ void calc_speed() {
         
 }
 
-void rollover_callback(){}
+void compute_pid()
+{
+    float error = (float)(setpoint - encoder_pulses);
+    i_term_result += i_term * error * (float)delay_time;
+    if (i_term_result > out_max) 
+        i_term_result = out_max;       // that the I term from PID gets too big
+    else if (i_term_result < out_min) 
+        i_term_result = out_min;
+        
+    int speed = (encoder_pulses - lastInput) / delay_time; 
+    output = p_term * error + i_term_result - d_term * speed;// PID output is the sum of P I and D values
+    
+    encoder_pulses_prev = encoder_pulses;
+    
+    if (output > 0){
+        if (output > out_max) 
+            output = out_max;     
+        motor_shield.setM1Speed((int)output + pwm_min);
+
+    }else{
+        if (output < out_min)
+            output = out_min;          
+        motor_shield.setM1Speed((int)output - pwm_min);
+    }
+    
+}
 
 
 void setup() {
@@ -64,42 +105,12 @@ void setup() {
     // configure interrupts and timers 
     attachInterrupt(digitalPinToInterrupt(ENC_PIN_1), encoder_ISR, FALLING);
     
-    TIM_TypeDef *tim_enc = (TIM_TypeDef *)pinmap_peripheral(digitalPinToPinName(ENC_PIN_1), PinMap_PWM);
-    uint32_t channel = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(ENC_PIN_1), PinMap_PWM));
-   
-/*   
-    HardwareTimer *tim_enc_isr = new HardwareTimer(tim_enc);
-    tim_enc_isr->setInterruptPriority(1,1);
-    tim_enc_isr->setMode(channel, TIMER_INPUT_CAPTURE_FALLING, ENC_PIN_1);
-    tim_enc_isr->setOverflow(0x10000);
-    tim_enc_isr->setPrescaleFactor(1);
-    tim_enc_isr->attachInterrupt(channel, encoder_ISR);
-    tim_enc_isr->attachInterrupt(rollover_callback);
-*/
-    HardwareTimer *tim_speed_calc = new HardwareTimer(TIM1);
-    tim_speed_calc->setInterruptPriority(2,2);
-    tim_speed_calc->setOverflow(1, HERTZ_FORMAT);
-    //tim_speed_calc->attachInterrupt(std::bind(calc_speed, &encoder_pulses));
-    tim_speed_calc->attachInterrupt(calc_speed);
-    
-    
-    tim_speed_calc->resume();
-    
-    //tim_enc_isr->resume();
-  
+
     motor_shield.init();
-    motor_shield.setM1Speed(200);
 }
 
 void loop() {
-    delay(1000);
-    Serial.println(encoder_pulses);
-    /*
-    int pulses = encoder_pulses - encoder_pulses_prev;
-    if (pulses != 0){
-        encoder_pulses_prev = encoder_pulses;
-        Serial.println(encoder_pulses);
-    }
-    motor_shield.getM1CurrentMilliamps();
-    */
+    delay(delay_time);
+    compute_pid();
+    
 }
