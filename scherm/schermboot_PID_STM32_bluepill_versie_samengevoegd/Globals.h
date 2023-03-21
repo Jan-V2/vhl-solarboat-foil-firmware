189e4099843875983e7daa8b91ad7deda323798e
RexvD <85557210+RexvD@users.noreply.github.com>
2023-03-21 21:07:37 +0100

    update

--- scherm/schermboot_PID_STM32_bluepill_versie_samengevoegd/Globals.h
+++ scherm/schermboot_PID_STM32_bluepill_versie_samengevoegd/Globals.h
@@ -2,6 +2,17 @@
 
 namespace Globals {
 
+enum class Menu : uint8_t {
+  OFF,
+  VOORVLEUGEL,
+  ACHTERVLEUGEL,
+  BALANS_VOORVLEUGEL,
+  DEBUG,
+  STARTUP
+};
+
+Menu menu;
+
 const float pi = 3.14159265359;
 
 } // namespace Globals
